9cb95dca43d28547fcb5059566c3f96be23094c9
Samuel Martinez <Samuel.Martinez1@wtwco.com>
2023-09-01 15:10:09 -0600

    Add project files.

---
+++ SuperLibrary/SimpleClass.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperLibrary
+{
+    public class SimpleClass
+    {
+        public string GetSomeInfo(string name)
+        {
+            var uppercase = name.ToUpper();
+            return $"hey {uppercase}!";
+        }
+
+    }
+}
